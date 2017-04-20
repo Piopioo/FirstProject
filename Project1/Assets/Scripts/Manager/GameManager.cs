ba500e43d4058f82844b78b54419a3ba872d7c7d
r.boumendil <r.boumendil@student.isartdigital.com>
2017-04-20 19:23:51 +0200

    Start to create Manager.
    Create a GameManager a BootScene and a GUIManager.

---
+++ Project1/Assets/Scripts/Manager/GameManager.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour {
+
+    public static GameManager _instance;
+    public GUIManager _GUIManager;
+
+    void Awake()
+    {
+        if (_instance == null)
+            _instance = this;
+        else if (_instance != this)
+            Destroy(gameObject);
+
+        DontDestroyOnLoad(gameObject);
+        DontDestroyOnLoad(_GUIManager);
+
+        InitGame();
+    }
+
+    void InitGame()
+    {
+
+    }
+}
---
+++ Project1/Assets/Scripts/Manager/Loader.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Loader : MonoBehaviour
+{
+    public GameObject _gameManager;
+
+    void Awake()
+    {
+        if (GameManager._instance == null)
+            Instantiate(_gameManager);
+
+        SceneManager. LoadScene(1);
+    }
+
+}
