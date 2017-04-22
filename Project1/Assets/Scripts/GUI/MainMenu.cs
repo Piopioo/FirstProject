d82d3c4a66a38a8fd958e1374b8178b9b1654b5c
r.boumendil <r.boumendil@student.isartdigital.com>
2017-04-22 21:17:39 +0200

    Start to create the main menu

---
+++ Project1/Assets/Scripts/GUI/MainMenu.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainMenu : MonoBehaviour {
+
+
+    public void StartGameButton()
+    {
+
+    }
+
+    public void OptionButton()
+    {
+
+    }
+
+    public void QuitGameButton()
+    {
+
+    }
+    
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
